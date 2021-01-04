1b8e84e913c999ab5f93af4de24cc8fc2e4084b3
wtycjan <56653553+wtycjan@users.noreply.github.com>
2021-01-04 17:31:32 +0100

    controller added

--- flaskrest/routes.py
+++ flaskrest/routes.py
@@ -1,64 +1,34 @@
-from datetime import datetime
+from flaskrest import app
+from flaskrest.controller import Controller
 
-from flask import flash, abort, jsonify, request, make_response
-from flask_login import current_user
-
-from flaskrest import app, db
-from flaskrest.models import UnityError
+controller = Controller()
 
 
 @app.route('/')
 @app.route('/errors')
 def showErrors():
-    errors = db.session.query(UnityError).all()
-    return make_response(jsonify(errors=[x.serialize for x in errors]), 200)
+    return controller.getAllErrors()
 
 
 @app.route('/errors/<int:error_id>')
 def showError(error_id):
-    errors = UnityError.query.get_or_404(error_id)
-    return make_response(jsonify(errors=errors.serialize), 200)
+    return controller.getSingleError(error_id)
 
 
 @app.route('/errors', methods=['GET', 'POST'])
 def newError():
-    line = request.form['line']
-    name = request.form['name']
-    description = request.form['description']
-    username = request.form['username']
-    newError = UnityError(line=line, name=name, description=description,
-                          date_posted=datetime.utcnow(), username=username)
-    db.session.add(newError)
-    db.session.commit()
-    print('Your post has been submitted.', 'success')
-
-    return make_response(jsonify(UnityError=newError.serialize), 201)
+    return controller.createNewError()
 
 
 @app.route("/errors/<int:error_id>", methods=['PUT', 'PATCH'])
 def editError(error_id):
-    updatedError = request.get_json()
-    updateError = UnityError.query.get_or_404(error_id)
-    if 'line' in updatedError:
-        updateError.line = updatedError['line']
-    if 'name' in updatedError:
-        updateError.name = updatedError['name']
-    if 'description' in updatedError:
-        updateError.description = updatedError['description']
-    db.session.commit()
-    return make_response(jsonify(UnityError=updateError.serialize), 200)
+    return controller.editError(error_id)
 
 
 @app.route('/errors/<int:error_id>', methods=['DELETE'])
 def deleteError(error_id):
-    deletedError = UnityError.query.get_or_404(error_id)
-    # if deletedError.author != current_user:
-    #     abort(403)
-    db.session.delete(deletedError)
-    db.session.commit()
-    print('Your error has been deleted!', 'success')
+    return controller.deleteError(error_id)
 
-    return make_response(jsonify({'result': True}), 204)
 
 # @app.route('/users')
 # def showUsers():
